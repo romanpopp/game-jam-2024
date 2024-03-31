c2d057d30e8c83202fc6fd918fd15b2da91daf31
Roman Popp <romanpopp00@gmail.com>
2024-03-30 20:16:26 -0500

    Created pickups, basic weapons, and a basic enemy

---
+++ logic/basicEnemy.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+# Movement variables
+@export var defaultSpeed = 1700
+var speed = defaultSpeed
+var accel = speed * 10
+@warning_ignore("integer_division")
+var friction = accel / speed
+var wandering = false
+var wanderDirection
+
+# Health
+@export var hp = 6
+var damage = 10
+
+@onready var player = get_parent().get_node("Player")
+
+func start(pos):
+	position = pos
+
+func _physics_process(delta):
+	var direction: Vector2 = player.position - position
+	rotation = direction.angle()
+	rotation -= PI / 2
+	
+	if wandering:
+		velocity += wanderDirection * speed * delta
+		velocity -= velocity * friction * delta
+	else:
+		velocity += direction.normalized() * speed * delta
+		velocity -= velocity * friction * delta
+	
+	var collision = move_and_collide(velocity * delta)
+	if collision != null && !wandering:
+		if collision.get_collider().is_in_group("Player"):
+			collision.get_collider().call("take_damage", damage) # makes player take damage
+		wanderDirection = -direction.normalized()
+		speed *= 1.5
+		wandering = true
+		$WanderTime.wait_time = randf_range(0.3, 0.5)
+		$WanderTime.start()
+
+# Makes this enemy take damage
+func take_damage(dmg):
+	hp -= dmg
+	if (hp <= 0): queue_free()
+
+# Knocks back the enemy
+func knock_back(mag):
+	velocity -= (player.position - position).normalized() * mag
+
+# Called when wander timer ends
+func _on_wander_time_timeout():
+	wandering = false
+	speed = defaultSpeed
--- logic/player.gd
+++ logic/player.gd
@@ -1,36 +1,128 @@
 extends CharacterBody2D
+const ID = preload("res://logic/idEnum.gd").ID
+const Weapon = preload("res://logic/idEnum.gd").Weapon
 
-# Visible in editor
-@export var speed = 5000
+# Speed and dodge settings
+@export var defaultSpeed = 5000
 @export var dodgeCD = 2
-@export var dodgeSpeed = 800
-
-@warning_ignore("integer_division")
+@export var dodgeSpeed = 5
+@export var speedMultiplier = 2
+var speed = defaultSpeed
 var accel = speed * 10
+@warning_ignore("integer_division")
 var friction = accel / speed
-
 var canDodge = true
 
+# HP settings
+@export var hp = 100
+
+# Stat buff variables
+var speedBoost = false
+var damageBoost = false
+@onready var boostParticles = get_node('BoostParticles')
+@onready var damageParticles = get_node('DamageParticles')
+
+# Weapon settings
+@export var projectileScene: PackedScene
+var currentWeapon = Weapon.default
+var canShoot = true
+var durability = 0
+
+# Set timer wait times
 func _ready():
 	$DodgeCD.wait_time = dodgeCD
 
+# Checking for button presses
 func _process(delta):
 	if Input.is_action_pressed("dodge"):
-		dodge(delta)
+		dodge()
+	if Input.is_action_pressed("shoot"):
+		shoot()
+	if durability <= 0:
+		durability = 0
+		currentWeapon = Weapon.default
+		$ShootCD.wait_time = 0.8
+	
+	# Durability for cigarette
+	if currentWeapon == Weapon.cigarette:
+		durability -= delta
 
+# Physics processing
 func _physics_process(delta):
+	if speed >= defaultSpeed:
+		boostParticles.emitting = true
+		speed -= 6
+	else: boostParticles.emitting = false
 	velocity += Input.get_vector("left", "right", "up", "down") * delta * speed
 	velocity -= velocity * delta * friction
 	move_and_slide()
 
 # Dodge function
-func dodge(delta):
-	if not canDodge:
+func dodge():
+	if !canDodge:
 		return
 	canDodge = false
-	velocity += velocity * dodgeSpeed * delta
 	$DodgeCD.start()
+	velocity += velocity * dodgeSpeed
+
+# Shoot function
+func shoot():
+	if !canShoot:
+		return
+	canShoot = false
+	$ShootCD.start()
+	match currentWeapon:
+		Weapon.default:
+			instantiate_projectiles(2)
+		Weapon.cigarette:
+			instantiate_projectiles(randi_range(4, 7))
+		Weapon.stapler:
+			instantiate_projectiles(1)
+			durability -= 1
+		Weapon.toaster:
+			pass
+
+# Gets ID enum from pickup
+func _on_pick_up(id):
+	match id:
+		ID.dayquil: # Health up and temporary speed boost
+			speed = defaultSpeed * speedMultiplier
+		ID.nyquil: # Greater health up and temporary damage boost
+			damageBoost = true
+			$DmgBoostTimer.start()
+			damageParticles.emitting = true
+		ID.cigarette: # Fires 3-5 ash projectiles that burn, has a timer
+			currentWeapon = Weapon.cigarette
+			durability = int(Weapon.cigarette)
+			$ShootCD.wait_time = 0.6
+		ID.stapler: # Fires large staples that knock back, has limited ammo
+			currentWeapon = Weapon.stapler
+			durability = int(Weapon.stapler)
+			$ShootCD.wait_time = 0.4
+		ID.toaster: # Melee flail weapon, has limited durability
+			currentWeapon = Weapon.toaster
+			durability = int(Weapon.toaster)
+		_: print("no ID")
+
+func instantiate_projectiles(n: int):
+	for i in range(0, n):
+		var projectile = projectileScene.instantiate()
+		get_tree().root.add_child(projectile)
+		projectile.start(currentWeapon, position, damageBoost)
+
+func take_damage(dmg):
+	hp -= dmg
+	print (hp)
 
 # Called by DodgeCD timer after timeout
 func _on_dodge_cd_timeout():
 	canDodge = true
+
+# Called by ShootCD timer after timeout
+func _on_shoot_cd_timeout():
+	canShoot = true
+
+
+func _on_dmg_boost_timeout():
+	damageBoost = false
+	damageParticles.emitting = false
